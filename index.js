a4e47fef3233385a429dfee18323c1546fd75a49
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-24 16:26:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
-
+// pulls a .target out of #nested
 }
 
 function increaseRankBy(n){
-
+// increases rank of .rankedlists by n - useparseInt()
 }
 
 function deepestChild(){
